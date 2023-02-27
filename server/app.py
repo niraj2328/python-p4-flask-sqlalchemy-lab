34bb5790ad62168e0d88e332afee4bfd7d2d2598
Niraj <niraj.patel@flatironschool.com>
2023-02-27 06:20:37 -0500

    completed

--- server/app.py
+++ server/app.py
@@ -17,17 +17,64 @@ db.init_app(app)
 def home():
     return '<h1>Zoo app</h1>'
 
+
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get_or_404(id)
+
+    response_body = f"""
+        <ul>Id: {animal.id}</ul>
+        <ul>Name: {animal.name}</ul>
+        <ul>Species: {animal.species}</ul>
+        <ul>Zookeeper: {animal.zookeeper}</ul>
+        <ul>Enclosure: {animal.enclosure}</ul>
+    """
+
+    return make_response(response_body, 200)
+
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get_or_404(id)
+
+    response_body = f"""
+        <ul>Id: {zookeeper.id}</ul>
+        <ul>Name: {zookeeper.name}</ul>
+        <ul>Birthday: {zookeeper.birthday}</ul>
+    """
+
+    animals = zookeeper.animals
+
+    if not animals:
+        response_body += f"<ul>No animals under care.</ul>"
+    else:
+        response_body += "<ul>Animals under care:</ul>"
+        for animal in animals:
+            response_body += f"<li>{animal.name}</li>"
+
+    return make_response(response_body, 200)
+
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.get_or_404(id)
+
+    response_body = f"""
+        <ul>Id: {enclosure.id}</ul>
+        <ul>Environment: {enclosure.environment}</ul>
+        <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>
+    """
+
+    animals = enclosure.animals
+
+    if not animals:
+        response_body += f"<ul>No animals in enclosure.</ul>"
+    else:
+        response_body += "<ul>Animals in enclosure:</ul>"
+        for animal in animals:
+            response_body += f"<li>{animal.name}</li>"
+
+    return make_response(response_body, 200)
 
 
 if __name__ == '__main__':
